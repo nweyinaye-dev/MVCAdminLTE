ff07928b7c62e1c4d7f727883617264911ee47c6
nweyinaye-11051997 <nweyinaye19970511@gmail.com>
2026-06-24 23:39:09 -0700

    Add project files.

---
+++ MVCAdminLTE/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCAdminLTE.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
